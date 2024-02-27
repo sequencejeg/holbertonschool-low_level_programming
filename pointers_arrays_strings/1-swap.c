3fdaa374921287ebf2dbe9459cf910dc71f0fd84
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-02-26 22:36:01 -0400

    Fixed 1-swap.c by adding the correct comment and descriptions 2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,9 +3,10 @@
 /**
  * swap_int - takes 2 pointer data types and switches their value
  *
- * @a: pointer
- * @b: pointer
  *
+ * @a: pointer 1
+ *
+ * @b: pointer 2
  */
 
 void swap_int(int *a, int*b)
