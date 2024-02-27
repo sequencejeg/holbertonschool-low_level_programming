3cf1a8109406cdce56c5483f31f76de22cc18bbf
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-02-26 22:24:19 -0400

    Adding Exercise 1: 1-swap.c after testing C code

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int data type and updates to 98
+ *
+ * @n: pointer
+ *
+ */
+
+void swap_int(int *a, int*b)
+
+{
+
+	*a = 42;
+	*b = 92;
+
+}
