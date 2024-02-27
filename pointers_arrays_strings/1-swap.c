e98d15e0d1bd67614a00b78c7ee44c201a5977c3
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-02-26 22:30:53 -0400

    Fixed 1-swap.c by adding the correct comment and descriptions

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int data type and updates to 98
+ * swap_int - takes 2 pointer data types and switches their value
  *
- * @n: pointer
+ * @a: pointer
+ * @b: pointer
  *
  */
 
