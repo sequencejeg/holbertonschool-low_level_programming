6990580c591582d6a246ea79467daf35980065df
Jeremy Etienne <sequence.jeg@Jeremys-MBP.localdomain>
2024-03-04 13:23:00 -0400

    New directory for projects. Adding Exercise 0, README, and .h

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - Prints a string followed by new line
+ * @s - String to be printed
+ *
+ * Return - void
+ *
+ */
+
+
+void _puts_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ recursion/README.md
@@ -0,0 +1,2 @@
+For the purpose of learning C-Recursion.
+Contains all project file exercises used in this lesson.
