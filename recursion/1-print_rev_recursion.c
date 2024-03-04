54ecbfe1b5749d44a550eb751464957076d5b0ae
Jeremy Etienne <sequence.jeg@Jeremys-MBP.localdomain>
2024-03-04 13:58:20 -0400

    Adding 1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Prints string in reverse
+ *
+ * @s - String to be printed
+ *
+ * Return - void
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
