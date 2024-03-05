4f751ea8f2192d797e65cdb66dc9f87873822a75
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-03-04 22:03:55 -0400

    Adding 2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * _strlen_recursion - Returns lengh of string
+ *
+ * @s: string
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+
+	else {
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
