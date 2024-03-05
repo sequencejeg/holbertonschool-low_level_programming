e1e1e350332828d32fa9922ce30cd8a75db7e67c
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-03-04 23:37:49 -0400

    Adding 4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *
+ * _pow_recursion - Returns value of x to the power of y
+ *
+ * @n: value of x
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	else if (y < 0)
+	{
+		return (0);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
