cb06b2a852f4fc93eb68c42b659da5b7edf94c5b
Jeremy Etienne <sequence.jeg@Jeremys-MacBook-Pro.local>
2024-03-05 14:11:08 -0400

    New Directory for argc & argv plus exercise 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Prints name followed by new line
+ *
+ * @argc: Number of arguments
+ *
+ * @*argv: Array of arguments
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,2 @@
+For better understanding and learning of int argc and char * argv[].
+Contains project files of exercises of said topic.
