b4d5a9eb18f69cdc5c352b2d4c1328096a34da39
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-03-05 22:22:57 -0400

    Adding 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed ro it
+ *
+ * @argc: number of arguments
+ *
+ * @argv: number of vectors
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
