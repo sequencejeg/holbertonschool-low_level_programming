71c31b9ffa759f966cf60dfac1d313d4c6c804f1
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-03-12 00:06:52 -0400

    Adding 2-print_dog modified v4 and new dog.h

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,22 +9,15 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-
-if (d->age == NULL)
-{
-printf("Age: (nil)");
-}
-
 if (d->name == NULL)
 {
 printf("Name: (nil)");
 }
-
 if (d->owner == NULL)
 {
 printf("Owner: (nil)");
 }
-
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-
+printf("Name: %s\n", d->name);
+printf("Age: %.f\n", d->age);
+printf("Owner: %s\n", d->owner);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
-/*
+/**
 * struct dog - structure for data needed
 * @name: Name of pet
 * @age: Age of pet
 * @owner: Owner of pet
 */
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
