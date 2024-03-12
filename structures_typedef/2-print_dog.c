e3371d253cebe2451e8798c104313f83f602f405
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-03-12 00:21:05 -0400

    Adding 2-print_dog modified v6

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,13 +8,17 @@
 void print_dog(struct dog *d)
 {
 if (d == NULL)
+{
 return;
+}
 if (d->name == NULL)
 {
+d->name = "(nil)"
 printf("Name: (nil)");
 }
 if (d->owner == NULL)
 {
+d->owner = "(nill)"
 printf("Owner: (nil)");
 }
 printf("Name: %s\n", d->name);
