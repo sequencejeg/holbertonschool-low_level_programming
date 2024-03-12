26541b60e21ba19db6dcc3a5ce4d442c3e9a2ff0
Jeremy Etienne <sequence.jeg@jeremys-mbp.lan>
2024-03-11 22:28:50 -0400

    Adding dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/*
+* struct dog - structure for data needed
+* @name: Name of pet
+* @age: Age of pet
+* @owner: Owner of pet
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
